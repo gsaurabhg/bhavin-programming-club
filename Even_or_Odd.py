faef01d21b52bd688eb18958ae2f6f11a7a97340
Bhavin Gupta <ironbhavin@gmail.com>
2020-06-30 22:44:49 +0530

    completed python hw-1 to detect if number is odd or even

---
+++ Even_or_Odd.py
@@ -0,0 +1,21 @@
+#Pseudo code: this code determines if input number is even or odd
+#ask user for the number
+# If input is not an whole Number, send error message
+#assign variable- NUM as answer,
+#Find remainder when NUM/2
+#If NUM mod 2=0:
+#  "The Number is Even"
+#If NUM mod 2 is not= 0
+#  "the number is odd"
+
+print('Hello! Please enter a number here so I can determine if its Even or Odd:  ')
+try:
+    NUM=int(input())
+    if NUM<0:
+        print('Please enter proper whole Number value!')
+    elif NUM%2==0:
+        print('The number '+str(NUM)+' is Even')
+    elif NUM%2!=0:
+        print('The number '+(str(NUM))+' is Odd')
+except ValueError:
+    print('Please enter proper whole Number value!')
